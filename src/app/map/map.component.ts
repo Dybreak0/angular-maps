70d8befe3e11f7f41d918a53350bba3836e2da33
Dybreak <sangutan.james@yahoo.com>
2023-05-26 15:44:03 +0800

    Add drawing module, and default location, add auto complete field to set starting location

--- src/app/map/map.component.ts
+++ src/app/map/map.component.ts
@@ -1,4 +1,4 @@
-import { AfterViewInit, ChangeDetectorRef, Component, OnInit, ViewChild } from '@angular/core';
+import { AfterViewInit, ChangeDetectorRef, Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { GoogleMap, MapInfoWindow, MapMarker } from '@angular/google-maps';
 
 @Component({
@@ -8,15 +8,14 @@ import { GoogleMap, MapInfoWindow, MapMarker } from '@angular/google-maps';
 })
 export class MapComponent implements OnInit, AfterViewInit {
  // TEST VARIABLE
-
+ @ViewChild(GoogleMap, { static: false }) map!: GoogleMap;
   @ViewChild(MapInfoWindow) private infoWindow!: MapInfoWindow;
-  @ViewChild(GoogleMap, { static: false }) map!: GoogleMap;
+  @ViewChild("autoCompleteField") autoCompleteField!: ElementRef;
 
   //#region Options
   public mapOptions!: google.maps.MapOptions;
   public polygonOptions!: google.maps.PolygonOptions;
-
-  //#endregion Map Options
+  //#endregion Options
 
   public vertices: google.maps.LatLngLiteral[] = [];
   public searchArea: google.maps.LatLngLiteral[] = [];
@@ -24,10 +23,9 @@ export class MapComponent implements OnInit, AfterViewInit {
 
   markers: any[] = [];
 
-  public showSearchPolygon: boolean = true;
-  public showSearchArea: boolean = false;
 
   public selectedPlace: any = null;
+  currentDrawing!: google.maps.Polygon;
 
   // List of available restaurant types
   public restaurantTypes: Array<any> = [
@@ -71,27 +69,17 @@ export class MapComponent implements OnInit, AfterViewInit {
   private directionsService!: google.maps.DirectionsService;
   private directionsRenderer!: google.maps.DirectionsRenderer;
   private placeService!: google.maps.places.PlacesService;
-  // public isPanelHidden: boolean = false;
 
-  // toggleSidebar() {
-  //   this.isPanelHidden = !this.isPanelHidden;
-  // }
+  public isDropdownFilter: boolean = false;
+  public isDropdownOptions: boolean = false;
 
-  isDropdownFilter: boolean = false;
-  isDropdownOptions: boolean = false;
-
-  public toggleDropdownFilter(): void {
-    this.isDropdownFilter = !this.isDropdownFilter;
-    this.isDropdownOptions = false;
+  private defaultLocation: google.maps.LatLngLiteral = {
+    lat: 10.309034,
+    lng: 123.893828
   }
 
-  public toggleDropdownOptions(): void {
-    this.isDropdownOptions = !this.isDropdownOptions;
-    this.isDropdownFilter = false;
-  }
-
+  private startingLocation!: google.maps.LatLngLiteral;
   constructor(private cdr: ChangeDetectorRef) {
-
   }
 
   ngOnInit(): void {
@@ -99,28 +87,10 @@ export class MapComponent implements OnInit, AfterViewInit {
 
   ngAfterViewInit(): void {
     this.initializeDependency();
+    this.initializeAutocomplete();
     this.initializeMap();
-    this.loadRestaurantData();
-  }
-
-  private initializeMap(): void {
-
-    this.mapOptions = {
-      zoom: 9,
-      center: {
-        lat: 10.3157,
-        lng: 123.8854
-      },
-      disableDoubleClickZoom: true,
-    }
-
-    this.polygonOptions = {
-      strokeColor: '#ED433B',
-      strokeOpacity: 0.8,
-      strokeWeight: 2,
-      fillColor: '#062E71',
-      fillOpacity: 0.25,
-    };
+    this.initializeDrawing();
+    this.initializeDefault();
   }
 
   private initializeDependency(): void {
@@ -133,50 +103,107 @@ export class MapComponent implements OnInit, AfterViewInit {
         strokeWeight: 8,
       }
     });
-
     this.placeService = new google.maps.places.PlacesService(<google.maps.Map>this.map.googleMap);
-
   }
 
-  private loadRestaurantData(): void {
+  private initializeAutocomplete(): void {
+    this.startingLocation = this.defaultLocation;
+    const autocomplete = new google.maps.places.Autocomplete(this.autoCompleteField.nativeElement);
+    autocomplete.addListener('place_changed', () => {
+      const place = autocomplete.getPlace();
+      const startLocation = place.geometry?.location;
+      this.startingLocation.lat = <number>startLocation?.lat()
+      this.startingLocation.lng = <number>startLocation?.lng()
+    });
+  }
+
+  private initializeMap(): void {
+    this.mapOptions = {
+      zoom: 9,
+      center: {
+        lat: 10.3157,
+        lng: 123.8854
+      },
+      disableDoubleClickZoom: true,
+    }
+  }
+
+  private initializeDrawing(): void {
+    this.polygonOptions = {
+      strokeColor: '#ED433B',
+      strokeOpacity: 0.8,
+      strokeWeight: 3,
+      fillColor: '#062E71',
+      fillOpacity: 0.25,
+    };
+
+    const drawingManager = new google.maps.drawing.DrawingManager({
+      drawingMode: null,
+      drawingControl: true,
+      drawingControlOptions: {
+        position: google.maps.ControlPosition.TOP_CENTER,
+        drawingModes: [google.maps.drawing.OverlayType.POLYGON]
+      },
+      polygonOptions: this.polygonOptions
+    });
+
+    drawingManager.setMap(<google.maps.Map>this.map.googleMap);
+
+    const $this = this;
+    google.maps.event.addListener(drawingManager, 'overlaycomplete', (event: google.maps.drawing.OverlayCompleteEvent) => {
+      // Clear previous drawing
+      if (this.currentDrawing) {
+        this.currentDrawing.setMap(null);
+      }
+
+      // Store the new drawing
+      this.currentDrawing = event.overlay as google.maps.Polygon;
+      const bounds = $this.getPolygonBounds(this.currentDrawing);
+      this.loadRestaurantData(bounds);
+    });
+  }
+
+  private initializeDefault(): void {
     // Polygon coordinates that covers whole cebu area
     this.searchArea = [
-      { lat: 9.384260, lng: 123.265764 },
-      { lat: 11.379277, lng: 123.247782 },
-      { lat: 11.418057, lng: 124.161251},
-      { lat: 9.352847, lng: 124.141142 }
+      { lat: 10.478004, lng: 123.735241 },
+      { lat: 10.481036, lng: 124.023496 },
+      { lat: 10.200497, lng: 124.014247},
+      { lat: 10.194428, lng: 123.746032 }
     ];
 
     // Create a LatLngBounds object from the polygon coordinates
     const bounds = new google.maps.LatLngBounds();
     this.searchArea.forEach((point: any) => bounds.extend(point));
 
+    this.loadRestaurantData(bounds);
+  }
+
+  private loadRestaurantData(bounds: google.maps.LatLngBounds): void {
     // Perform a search within the polygon bounds
+    this.markers = [];
     const request: google.maps.places.TextSearchRequest = {
       bounds: bounds,
-      query: 'cebu restaurants',
+      query: 'restaurants',
       type: "restaurant"
     };
 
     this.placeService.textSearch(request, (results, status, pagination) => {
       if (status === google.maps.places.PlacesServiceStatus.OK) {
-        // Process the search results here
-        console.log(results);
         this.searchResults = <google.maps.places.PlaceResult[]>results;
-        this.plotMarker(results, pagination);
+        this.plotMarker(results);
       }
     });
     this.cdr.detectChanges();
   }
 
-  plotMarker(results: any, pagination?: any) {
+  plotMarker(results: any) {
     if(results.length > 0) {
       // Fit the map bounds to the markers
       const bounds = new google.maps.LatLngBounds();
       for (const place of results) {
         bounds.extend(place.geometry?.location);
         // Process each restaurant place
-        console.log(place.name, place.formatted_address);
         var coord: any = place?.geometry?.location?.toJSON();
         if (coord != null)  {
           this.markers.push({
@@ -193,24 +220,15 @@ export class MapComponent implements OnInit, AfterViewInit {
         }
       }
       this.map.fitBounds(bounds);
-      this.vertices = this.convertBoundsToLiteral(bounds);
     }
     else {
-      this.vertices = [];
-      this.map.panTo({ lat: 10.3157, lng: 123.8854 }); // Replace with desired center coordinates
+      this.map.panTo({ lat: 10.3157, lng: 123.8854 });
     }
-    // if (pagination && pagination.hasNextPage) {
-    //   // If there is a next page, retrieve the next page of results
-    //   pagination.nextPage();
-    // }
-
     this.cdr.detectChanges();
-
   }
 
   openInfoWindow(marker: MapMarker, markerItem: any) {
     if (this.infoWindow != undefined) {
-    // Define the request for place details
       const request = {
         placeId: markerItem.placeId,
       };
@@ -218,13 +236,6 @@ export class MapComponent implements OnInit, AfterViewInit {
       // Call the PlacesService getDetails method
       this.placeService.getDetails(request, (placeDetails, status) => {
         if (status === google.maps.places.PlacesServiceStatus.OK) {
-          // Check if the cuisine information is available
-          // if (place?.cuisine) {
-          //   console.log(place.cuisine); // Print the cuisine information
-          // } else {
-          //   console.log('Cuisine information not available.');
-          // }
-          console.log(placeDetails);
           this.selectedPlace = placeDetails;
           this.infoWindow.open(marker);
           this.cdr.detectChanges();
@@ -265,8 +276,8 @@ export class MapComponent implements OnInit, AfterViewInit {
   getDirections(): void {
     navigator.geolocation.getCurrentPosition(
       (position) => {
-        const currentLat = position.coords.latitude;
-        const currentLng = position.coords.longitude;
+        const currentLat = this.startingLocation.lat;
+        const currentLng = this.startingLocation.lng;
 
         const request = {
           origin: { lat: currentLat, lng: currentLng },
@@ -278,7 +289,6 @@ export class MapComponent implements OnInit, AfterViewInit {
           if (status === google.maps.DirectionsStatus.OK) {
             this.directionsRenderer.setDirections(result);
 
-            // Gradually zoom out after setting directions
             let zoomLevel = <number>this.map.getZoom();
             const zoomInterval = setInterval(() => {
               if (zoomLevel > 0) {
@@ -289,14 +299,31 @@ export class MapComponent implements OnInit, AfterViewInit {
               } else {
                 clearInterval(zoomInterval);
               }
-            }, 100); // Adjust the interval duration as needed
+            }, 100);
           }
         });
       },
       (error) => {
-        // Handle error cases
         console.log(error);
       }
     );
+  }
+
+  public toggleDropdownFilter(): void {
+    this.isDropdownFilter = !this.isDropdownFilter;
+    this.isDropdownOptions = false;
+  }
+
+  private getPolygonBounds(polygon: google.maps.Polygon) {
+    const bounds = new google.maps.LatLngBounds();
+    const paths = polygon.getPaths();
+
+    paths.forEach((path) => {
+      path.forEach((latLng) => {
+        bounds.extend(latLng);
+      });
+    });
+
+    return bounds;
   }
 }
